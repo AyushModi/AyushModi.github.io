115cc0dd2934171435460c8790e89178a8dd8091
Ayush Modi <amodi252@gmail.com>
2024-07-11 18:23:47 +0100

    backend

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,4 +1,4 @@
 const action_name = "action_hello_world";
-//const rasa_server_url = "https://backend.hotelschatbot.com/webhooks/rest/webhook"; // use this for backend chatbot
-const rasa_server_url = "https://frontend.hotelschatbot.com/webhooks/rest/webhook"; // use this for book room chatbot
+const rasa_server_url = "https://backend.hotelschatbot.com/webhooks/rest/webhook"; // use this for backend chatbot
+//const rasa_server_url = "https://frontend.hotelschatbot.com/webhooks/rest/webhook"; // use this for book room chatbot
 const sender_id = uuidv4();
